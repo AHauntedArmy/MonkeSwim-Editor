e289ad25c73f91f2034d6f7ca3480e3c8e585bef
AHauntedArmy <extreme15@hotmail.co.uk>
2022-01-22 02:46:32 +0000

    added object toggle triggers

--- MMM archive.ps1
+++ MMM archive.ps1
@@ -1,6 +1,6 @@
 # Needs to be at least that version, or mmm can't read the archive
 #Requires -Modules @{ ModuleName="Microsoft.PowerShell.Archive"; ModuleVersion="1.2.3" }
 $Name = "MonkeSwim-Editor" # Replace with your mods name
-$Version = "v1.0.0"
+$Version = "v1.0.1"
 
 Compress-Archive -force .\Assets\ $Name-$Version.zip
