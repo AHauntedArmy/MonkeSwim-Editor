2469cf41bb9e289bf326d13acdab78632bd19912
AHauntedArmy <extreme15@hotmail.co.uk>
2022-03-20 17:13:52 +0000

    added invert rotation option

--- MMM archive.ps1
+++ MMM archive.ps1
@@ -1,6 +1,6 @@
 # Needs to be at least that version, or mmm can't read the archive
 #Requires -Modules @{ ModuleName="Microsoft.PowerShell.Archive"; ModuleVersion="1.2.3" }
 $Name = "MonkeSwim-Editor" # Replace with your mods name
-$Version = "v1.0.1"
+$Version = "v1.0.2"
 
 Compress-Archive -force .\Assets\ $Name-$Version.zip
